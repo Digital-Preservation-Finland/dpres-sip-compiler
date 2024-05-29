deb2dda3e872308c4b19b69d9e07da610b39fe8e
Tiina Vento <tiina.vento@csc.fi>
2024-05-29 14:52:56 +0300

    KDKPAS-3254 Add test files and write a test for compiler-ng

---
+++ tests/compiler_ng_test.py
@@ -0,0 +1,20 @@
+"""Test SIP compilation with compiler-ng.
+"""
+import os
+import pytest
+from dpres_sip_compiler.compiler_ng import ng_compile_sip
+
+
+def test_ng_compile_sip(tmpdir, pick_files_tar):
+    """Test sip compilation."""
+    tar_file = os.path.join(str(tmpdir), "test_sip.tar")
+    ng_compile_sip("tests/data/compiler_ng/files",
+                   "tests/data/compiler_ng/desc_dc_metadata.xml",
+                   conf_file="tests/data/compiler_ng/generic.conf",
+                   tar_file=tar_file)
+    assert os.path.isfile(tar_file)
+    tar_list = pick_files_tar(tar_file)
+    assert "mets.xml" in tar_list
+    assert "signature.sig" in tar_list
+    assert "test_file_01.txt" in tar_list
+    assert "test_file_02.txt" in tar_list

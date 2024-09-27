a57e1299488f3d0d6703a0701ebe97db5d09a069
Johan Kylander <johan.kylander@csc.fi>
2024-09-27 06:15:46 +0000

    KDKPAS-3336 Fix compile-ng command to work with the refactored siptools-ng library

--- dpres_sip_compiler/compiler_ng.py
+++ dpres_sip_compiler/compiler_ng.py
@@ -1,20 +1,21 @@
 """Compile SIP using dpres-siptools-ng."""
 
 import os
-from mets_builder import METS, MetsProfile, StructuralMap
+from mets_builder import METS, MetsProfile
 from mets_builder.metadata import ImportedMetadata
 
 from siptools_ng.sip import SIP
-from siptools_ng.sip_digital_object import SIPDigitalObject
+from siptools_ng.file import File
 
 from dpres_sip_compiler.base_adaptor import build_sip_metadata
 from dpres_sip_compiler.adaptor_list import ADAPTOR_NG_DICT
 from dpres_sip_compiler.config import Config, get_default_config_path
 
 
+# pylint: disable=too-many-instance-attributes, too-few-public-methods
 class SipCompiler:
     """Class to compile SIP."""
-    # pylint: disable=too-many-arguments
+    # pylint: disable=too-many-arguments, too-many-positional-arguments
     def __init__(self, source_path, descriptive_metadata_path,
                  config, tar_file, sip_meta):
         self.source_path = source_path
@@ -25,7 +26,6 @@ class SipCompiler:
         self.mets = None
         self.digital_objects = []
         self.descriptive_metadata = None
-        self.structural_map = None
 
     def _initialize_mets(self):
         """Initialize dpres-mets-builder METS object with your
@@ -41,9 +41,10 @@ class SipCompiler:
     def _create_technical_metadata(self):
         """Create digital objects and add metadata to them."""
         for obj in self.sip_meta.objects:
-            digital_object = SIPDigitalObject(
-                source_filepath=obj.filepath,
-                sip_filepath=os.path.relpath(obj.filepath, self.source_path)
+            digital_object = File(
+                path=obj.filepath,
+                digital_object_path=os.path.relpath(
+                    obj.filepath, self.source_path)
             )
             digital_object.generate_technical_metadata()
             self.digital_objects.append(digital_object)
@@ -58,19 +59,11 @@ class SipCompiler:
             metadata_format=self.config.desc_metadata_format,
             format_version=self.config.desc_metadata_version)
 
-    def _compile_structural_map(self):
-        """Create directory based structural map and add the imported
-        descriptive metadata to it.
-        """
-        self.structural_map = StructuralMap.from_directory_structure(
-            self.digital_objects)
-        self.mets.add_structural_map(self.structural_map)
-        self.structural_map.root_div.add_metadata(self.descriptive_metadata)
-
     def _finalize_sip(self):
         """Turn the METS object into a SIP."""
 
-        sip = SIP(mets=self.mets)
+        sip = SIP.from_files(mets=self.mets, files=self.digital_objects)
+        sip.add_metadata([self.descriptive_metadata])
         sip.finalize(
             output_filepath=self.tar_file,
             sign_key_filepath=self.config.sign_key
@@ -81,8 +74,6 @@ class SipCompiler:
         self._initialize_mets()
         self._create_technical_metadata()
         self._import_descriptive_metadata()
-        self._compile_structural_map()
-        self.mets.generate_file_references()
         self._finalize_sip()
         print(f"Compilation finished. The SIP is signed and packaged to: "
               f"{self.tar_file}.")
